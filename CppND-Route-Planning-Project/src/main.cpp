72ead941318ec7727b651ebca8eba124a84ef966
Zhi Yuan <zhi.yuan@nokia-sbell.com>
2019-10-23 18:03:03 +0800

    TODO 1 done

--- CppND-Route-Planning-Project/src/main.cpp
+++ CppND-Route-Planning-Project/src/main.cpp
@@ -55,12 +55,24 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout<<"The map coordinates start from (0,0) in the bottom left and end at (100,100) in the top right." << "\n";
+    std::cout<<"Enter start_x between 0 and 100 : ";
+    std::cin>>start_x;
 
+    std::cout<<"Enter start_y between 0 and 100 : ";
+    std::cin>>start_y;
+
+    std::cout<<"Enter end_x between 0 and 100 : ";
+    std::cin>>end_x;
+
+    std::cout<<"Enter end_y between 0 and 100 : ";
+    std::cin>>end_y;
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
